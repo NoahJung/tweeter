bfd92f4cfab9fe477a706b9d73763b7775d29550
Minjae Jung <noahjung87@gmail.com>
2021-03-29 02:08:31 +0000

    comment added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,20 +7,21 @@
 
 $(document).ready(function() {
 
+  // for preventing script text from tweets
   const escape =  function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   }
 
+  // tweet list rendering
   const renderTweets = function(tweets) {
-
     for (const item of tweets) {
       $('#all-tweets').prepend(createTweetElement(item));
     }
-
   }
 
+  // print a tweet article
   const createTweetElement = function(tweet) {
     let $tweet = `<a class="effect">
       <article class="tweet">
@@ -42,19 +43,17 @@ $(document).ready(function() {
     return $tweet;
   }
 
+  // Get tweet data as json, invoke renderTweets
   const loadTweets = function() {
-    // $.ajax('/tweets', { method: 'GET' })
-    // .then(function() {
-    //   renderTweets(data);
-    // });
     $.getJSON( "/tweets", function(data){
       renderTweets(data);
     });
-
   }
 
-  loadTweets();
+  loadTweets();   // load tweet when index page is ready.
 
+
+  // new-tweet submit event
   $("form").on( "submit", function( event ) {
     event.preventDefault();
 
@@ -62,28 +61,31 @@ $(document).ready(function() {
     const errAlert = $('.errorM');
     errAlert.css("display","none");
    
-    if (text.length === 0 || !text.trim()) {
+    if (text.length === 0 || !text.trim()) {              // when the tweet text is whitespace
       errAlert.text("Please write something to tweet!")
               .css("display","block").hide().slideDown();
-    } else if (text.length > 140) {
+    } else if (text.length > 140) {                           // when the tweet text is over 140 characters limit
       errAlert.text("Your tweet exceeds the 140 character limit")
               .css("display","block").hide().slideDown();
     } else {
+      // post submit data 
       $.ajax({
         url: '/tweets',
         dataType: 'text',
         method: 'post',
         contentType: 'application/x-www-form-urlencoded',
-        data: $(this).serialize()
+        data: $(this).serialize()                       // data formating : query string
       }).then(function(){
-        loadTweets();
-        $("textarea").val('');
-        $("output").text("140");
+        loadTweets();                     // reload tweet list with new tweet data
+        $("textarea").val('');            // clear textarea
+        $("output").text("140");          // reset counter
       })
     }
 
   });
 
+
+  // compose button on nav
   $(".composeBT").on( "click", function( event ){
     event.preventDefault();
 
